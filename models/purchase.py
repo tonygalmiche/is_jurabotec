bc6bc488a292376d4c9ef6ba812b3348a5ea0793
Tony Galmiche <tony.galmiche@infosaone.com>
2023-02-04 17:17:50 +0100

    modifs

--- models/purchase.py
+++ models/purchase.py
@@ -5,7 +5,7 @@ from odoo import fields, models, api
 class IsContratFournisseurLigne(models.Model):
     _name='is.contrat.fournisseur.ligne'
     _description = "Lignes contrat fournisseur"
-
+    _rec_name = "id"
 
     @api.depends('qt_prevue','prix_achat')
     def _compute_montant(self):
@@ -16,10 +16,26 @@ class IsContratFournisseurLigne(models.Model):
             obj.montant = montant
 
 
+    def _compute_qt_commandee(self):
+        for obj in self:
+            filtre=[
+                ('is_contrat_id', '=', obj.contrat_id.id),
+                ('state','in',["purchase"]),
+            ]
+            orders = self.env['purchase.order'].search(filtre)
+            qt = 0
+            for order in orders:
+                for line in order.order_line:
+                    if line.product_id.product_tmpl_id==obj.product_id:
+                        qt+=line.is_volume_total
+            obj.qt_commandee = qt
+
+
     contrat_id   = fields.Many2one('is.contrat.fournisseur', 'Contrat', required=True, ondelete='cascade')
     product_id   = fields.Many2one('product.template', 'Article', required=True)
-    qt_prevue    = fields.Float(string="Quantité prévue", digits='Product Unit of Measure', required=True)
-    prix_achat   = fields.Float(string="Prix d'achat"   , digits='Product Unit of Measure', required=True)
+    qt_prevue    = fields.Float(string="Quantité prévue"   , digits='Product Unit of Measure', required=True)
+    qt_commandee = fields.Float(string="Quantité commandée", digits='Product Unit of Measure', compute='_compute_qt_commandee')
+    prix_achat   = fields.Float(string="Prix d'achat"      , digits='Product Unit of Measure', required=True)
     unite = fields.Selection([
         ('m' , 'm'),
         ('m2', 'm2'),
@@ -65,11 +81,23 @@ class IsContratFournisseur(models.Model):
 class PurchaseOrder(models.Model):
     _inherit = "purchase.order"
 
-    is_contrat_id = fields.Many2one('is.contrat.fournisseur', 'Contrat')
+    is_contrat_id = fields.Many2one('is.contrat.fournisseur', 'Contrat', index=True)
 
 
 class PurchaseOrderLine(models.Model):
     _inherit = "purchase.order.line"
+
+    is_ligne_contrat_id = fields.Many2one('is.contrat.fournisseur.ligne', 'Ligne contrat')
+    is_qt_contrat       = fields.Float(string="Qt contrat", digits='Product Unit of Measure')
+    is_prix_contrat     = fields.Float(string="Prix contrat"   , digits='Product Unit of Measure')
+    is_unite_contrat    = fields.Selection([
+        ('m' , 'm'),
+        ('m2', 'm2'),
+        ('m3', 'm3'),
+    ], "Unité contrat")
+    is_volume       = fields.Float(string="Volume", related="product_id.is_volume", readonly=True)
+    is_volume_total = fields.Float(string="Volume total", digits='Product Unit of Measure', compute='_compute_is_volume_total')
+
 
 
     def _compute_price_unit_and_date_planned_and_name(self):
@@ -82,3 +110,25 @@ class PurchaseOrderLine(models.Model):
                         price_unit = line.prix_achat*self.product_id.is_volume
         if price_unit:
             self.price_unit = price_unit
+
+
+    @api.depends('is_volume','product_qty')
+    def _compute_is_volume_total(self):
+        for obj in self:
+            obj.is_volume_total = obj.product_qty*obj.is_volume
+
+
+    @api.onchange('product_id')
+    def _onchange_product_id_contrat(self):
+        self.is_ligne_contrat_id = False
+        self.is_qt_contrat       = False
+        self.is_prix_contrat     = False
+        self.is_unite_contrat    = False
+        if self.product_id:
+            for line in self.order_id.is_contrat_id.ligne_ids:
+                if line.product_id == self.product_id.product_tmpl_id:
+                    self.is_ligne_contrat_id = line.id
+                    self.is_qt_contrat       = line.qt_prevue
+                    self.is_prix_contrat     = line.prix_achat
+                    self.is_unite_contrat    = line.unite
+                    break
