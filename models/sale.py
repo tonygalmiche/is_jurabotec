9e1a6cdf85a45df237ed84d29cd70ba25c84c402
Tony Galmiche <tony.galmiche@infosaone.com>
2023-02-25 17:19:32 +0100

    modifs

--- models/sale.py
+++ models/sale.py
@@ -143,6 +143,45 @@ class sale_order_line(models.Model):
 
 
 
+    @api.depends('product_id', 'product_uom', 'product_uom_qty','is_prix_tarif','is_unite_tarif','product_uom_qty')
+    def _compute_price_unit(self):
+        for line in self:
+
+            price = 0
+            if line.is_unite_tarif=="m":
+                price = line.is_prix_tarif*line.is_longueur
+            if line.is_unite_tarif=="m2":
+                price = line.is_prix_tarif*line.is_surface
+            if line.is_unite_tarif=="m3":
+                price = line.is_prix_tarif*line.is_volume
+            if line.is_unite_tarif=="unite":
+                price = line.is_prix_tarif
+            line.price_unit = price
+
+
+
+            # # check if there is already invoiced amount. if so, the price shouldn't change as it might have been
+            # # manually edited
+            # if line.qty_invoiced > 0:
+            #     continue
+            # if not line.product_uom or not line.product_id or not line.order_id.pricelist_id:
+            #     line.price_unit = 0.0
+            # else:
+            #     price = line.with_company(line.company_id)._get_display_price()
+            #     line.price_unit = line.product_id._get_tax_included_unit_price(
+            #         line.company_id,
+            #         line.order_id.currency_id,
+            #         line.order_id.date_order,
+            #         'sale',
+            #         fiscal_position=line.order_id.fiscal_position_id,
+            #         product_price_unit=price,
+            #         product_currency=line.currency_id
+            #     )
+
+
+
+
+
 
     @api.onchange('product_id','product_template_id')
     def _onchange_product_id(self):
@@ -165,18 +204,18 @@ class sale_order_line(models.Model):
 
 
 
-    @api.onchange('is_prix_tarif','is_unite_tarif','product_uom_qty')
-    def _onchange_is_prix_tarif(self):
-        price = 0
-        if self.is_unite_tarif=="m":
-            price = self.is_prix_tarif*self.is_longueur
-        if self.is_unite_tarif=="m2":
-            price = self.is_prix_tarif*self.is_surface
-        if self.is_unite_tarif=="m3":
-            price = self.is_prix_tarif*self.is_volume
-        if self.is_unite_tarif=="unite":
-            price = self.is_prix_tarif
-        self.price_unit = price
+    # @api.onchange('is_prix_tarif','is_unite_tarif','product_uom_qty')
+    # def _onchange_is_prix_tarif(self):
+    #     price = 0
+    #     if self.is_unite_tarif=="m":
+    #         price = self.is_prix_tarif*self.is_longueur
+    #     if self.is_unite_tarif=="m2":
+    #         price = self.is_prix_tarif*self.is_surface
+    #     if self.is_unite_tarif=="m3":
+    #         price = self.is_prix_tarif*self.is_volume
+    #     if self.is_unite_tarif=="unite":
+    #         price = self.is_prix_tarif
+    #     self.price_unit = price
 
 
 
