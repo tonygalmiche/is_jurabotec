d6aa610d6c37d413220b14494a25ad52d95257f2
Tony Galmiche <tony.galmiche@infosaone.com>
2023-10-10 10:31:47 +0200

    ùoidifs

--- models/stock.py
+++ models/stock.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
-from odoo import fields, models, api
+from odoo import _, api, fields, models
+from odoo.tools.float_utils import float_compare, float_is_zero
+from odoo.exceptions import UserError, ValidationError
 
 
 # class StockQuant(models.Model):
@@ -198,6 +200,50 @@ class StockQuant(models.Model):
             }
 
 
+
+
+
+
+    @api.model
+    def _update_reserved_quantity(self, product_id, location_id, quantity, lot_id=None, package_id=None, owner_id=None, strict=False):
+        self = self.sudo()
+        rounding = product_id.uom_id.rounding
+        quants = self._gather(product_id, location_id, lot_id=lot_id, package_id=package_id, owner_id=owner_id, strict=strict)
+        reserved_quants = []
+
+        if float_compare(quantity, 0, precision_rounding=rounding) > 0:
+            # if we want to reserve
+            available_quantity = sum(quants.filtered(lambda q: float_compare(q.quantity, 0, precision_rounding=rounding) > 0).mapped('quantity')) - sum(quants.mapped('reserved_quantity'))
+            #if float_compare(quantity, available_quantity, precision_rounding=rounding) > 0:
+            #    raise UserError(_('It is not possible to reserve more products of %s than you have in stock.', product_id.display_name))
+        elif float_compare(quantity, 0, precision_rounding=rounding) < 0:
+            # if we want to unreserve
+            available_quantity = sum(quants.mapped('reserved_quantity'))
+            #if float_compare(abs(quantity), available_quantity, precision_rounding=rounding) > 0:
+            #    raise UserError(_('It is not possible to unreserve more products of %s than you have in stock.', product_id.display_name))
+        else:
+            return reserved_quants
+
+        for quant in quants:
+            if float_compare(quantity, 0, precision_rounding=rounding) > 0:
+                max_quantity_on_quant = quant.quantity - quant.reserved_quantity
+                if float_compare(max_quantity_on_quant, 0, precision_rounding=rounding) <= 0:
+                    continue
+                max_quantity_on_quant = min(max_quantity_on_quant, quantity)
+                quant.reserved_quantity += max_quantity_on_quant
+                reserved_quants.append((quant, max_quantity_on_quant))
+                quantity -= max_quantity_on_quant
+                available_quantity -= max_quantity_on_quant
+            else:
+                max_quantity_on_quant = min(quant.reserved_quantity, abs(quantity))
+                quant.reserved_quantity -= max_quantity_on_quant
+                reserved_quants.append((quant, -max_quantity_on_quant))
+                quantity += max_quantity_on_quant
+                available_quantity += max_quantity_on_quant
+
+            if float_is_zero(quantity, precision_rounding=rounding) or float_is_zero(available_quantity, precision_rounding=rounding):
+                break
+        return reserved_quants
 
 
 
